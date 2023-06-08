1ce43ac41d87f4fe3107d60749c030f6c64ce9a1
NVT-Game <toainguyenprogramminggame@gmail.com>
2023-06-08 16:30:06 +0700

    change path

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ done
 
 [ -z "$SEMVER" ] && echo "You must provide the --semver option." && exit 1
 
-PREFIX="Assets/AdBase"
+PREFIX="Assets/CharacterController2D"
 BRANCH="upm"
 
 git subtree split --prefix="$PREFIX" --branch $BRANCH
