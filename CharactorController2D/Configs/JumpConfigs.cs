e169e6070c7a81bd4ce98190e78c71c5ec3f822b
NVT-Game <toainguyenprogramminggame@gmail.com>
2023-07-02 19:20:39 +0700

    update package to 1.0.5

--- CharactorController2D/Configs/JumpConfigs.cs
+++ CharactorController2D/Configs/JumpConfigs.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "JumpConfig", menuName = "CharacterController/Jump", order = 1)]
 public class JumpConfigs : ScriptableObject
 {
-    [SerializeField] private float jumpForce;
-    [SerializeField] private float fallMultiplier;
-    [SerializeField] private float groundCheckRadius;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private int extraJumpCount = 1;
-    [SerializeField] private GameObject jumpEffect;
+    [SerializeField] protected float jumpForce;
+    [SerializeField] protected float fallMultiplier;
+    [SerializeField] protected float groundCheckRadius;
+    [SerializeField] protected LayerMask groundLayer;
+    [SerializeField] protected int extraJumpCount = 1;
+    [SerializeField] protected GameObject jumpEffect;
+    [SerializeField] protected bool canMoveWhenJump = true;
 
     public float JumpForce { get { return jumpForce; } }
     public float FallMultiplier { get {  return fallMultiplier; } }
